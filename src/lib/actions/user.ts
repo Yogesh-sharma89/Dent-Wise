bbef75667cb3667c133a3d77ca1fc5c78c687201
Yogesh Sharma <nitya.upmanyu@gmail.com>
2025-11-10 11:21:29 +0530

    check email

---
+++ src/lib/actions/user.ts
@@ -0,0 +1,45 @@
+'use server'
+
+import { currentUser } from "@clerk/nextjs/server";
+import {prisma} from '../prisma';
+
+
+
+export async function AddUserToDb(){
+
+    try{
+
+        const user  = await currentUser();
+
+        if(!user){
+            console.log('user not authenticated')
+            return ;
+        }
+
+        //If user is authenticated then check if user alreday in Db or not 
+
+        const existingUser = await prisma.user.findUnique({ where:{clerkId : user.id}})
+
+        if(existingUser) return existingUser;
+
+
+        if(!user.primaryEmailAddress?.emailAddress){
+            throw new Error('Clerk dont provide any email');
+        }
+
+        const newUser  = await prisma.user.create({
+            data:{
+                clerkId:user.id,
+                firstName:user.firstName,
+                lastName:user.lastName,
+                email:user.primaryEmailAddress?.emailAddress   ,
+                phone:user.primaryPhoneNumber?.phoneNumber,
+            }
+        })
+
+        return newUser;
+
+    }catch(err){
+        console.log("Error in addusertoDb function : "+err);
+    }
+}
